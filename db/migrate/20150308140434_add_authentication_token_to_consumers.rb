510e1faca081a85e1b8c45ce3e059fb857a77653
Guoqing <zhaogq10@163.com>
2015-03-08 22:31:39 +0800

    consumer token authentication: migration

---
+++ db/migrate/20150308140434_add_authentication_token_to_consumers.rb
@@ -0,0 +1,6 @@
+class AddAuthenticationTokenToConsumers < ActiveRecord::Migration
+  def change
+    add_column :consumers, :authentication_token, :string
+    add_index :consumers, :authentication_token
+  end
+end
