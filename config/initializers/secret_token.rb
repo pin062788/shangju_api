9079335a8c1840132082848b42846a1e124c5e39
Guoqing <zhaogq10@163.com>
2015-03-08 22:29:31 +0800

    add token_authenticatable to consumer

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TopTalTimeZones::Application.config.secret_key_base = 'd7b80ce34fea6a1dc5a92a2c3a7641df990f50fe8e919b43f1c69353e7db7f6e777a1e97644ae30df67ae932fc5076c5488331a4f87576ec7765ac405620046d'
